9e86e78cbda56ac7c4e3cf457fa100834d3cf9fd
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-12 18:57:59 +0300

    Refactor LoginForm component to use reusable Input and SubmitButton components

--- src/ui/LoginForm.jsx
+++ src/ui/LoginForm.jsx
@@ -3,6 +3,8 @@ import FormUi from "./FormUi";
 import { signInSchema } from "../validation/validation";
 import useFormData from "../hooks/useFormData";
 import ErrorInputFieldMassage from "./ErrorInputFieldMassage";
+import { Input } from "postcss";
+import SubmitButton from "./SubmitButton";
 
 const initialData = {
   email: "",
@@ -34,46 +36,19 @@ function LoginForm() {
       </FormUi.Header>
 
       <FormUi.Body>
-        <label
-          className={`flex flex-col gap-1 ${
-            validationErrors?.email ? "text-red-500" : ""
-          }`}
-        >
+        <Input error={validationErrors?.email} type="email" name={"email"}>
           Email
-          <input
-            name="email"
-            className={`border-2 outline-none  py-2 px-2 rounded-lg ${
-              validationErrors?.email ? "border-red-500" : ""
-            }`}
-            type="email"
-          />
-          <ErrorInputFieldMassage>
-            {validationErrors?.email}
-          </ErrorInputFieldMassage>
-        </label>
-        <label
-          className={`flex flex-col gap-1 ${
-            validationErrors?.password ? "text-red-500" : ""
-          }`}
+        </Input>
+
+        <Input
+          error={validationErrors?.password}
+          type="password"
+          name={"password"}
         >
           Password
-          <input
-            name="password"
-            className={`border-2 py-2 px-2 rounded-lg ${
-              validationErrors?.password ? "border-red-500" : ""
-            }`}
-            type="password"
-          />
-          <ErrorInputFieldMassage>
-            {validationErrors?.password}
-          </ErrorInputFieldMassage>
-        </label>
-        <button
-          type="submit"
-          className="bg-primary text-[#fff] py-2 rounded-lg"
-        >
-          Login
-        </button>
+        </Input>
+
+        <SubmitButton>Login</SubmitButton>
       </FormUi.Body>
       <FormUi.Footer>
         Create new account?&nbsp;

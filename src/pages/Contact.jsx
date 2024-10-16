891d61b1f3a4c6a3351aa92ee06c8a504f6c3aa8
Rokaia-ramadan18 <rokaiaramadan78@gmail.com>
2024-10-16 21:36:07 +0300

    i have built contact commponent

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,38 @@
 function Contact() {
-  return <div>Contact</div>;
+return (
+  <div>
+    <div className=" text-center text-2xl pt-10 text-gray-500">
+      <p>
+        CONTACT <span className=" text-gray-700 font-semibold">US</span>
+      </p>
+    </div>
+    <div className=" my-10 flex flex-col justify-center md:flex-row gap-10 mb-28  ">
+      <img
+        className=" w-full md:max-w-[360px]"
+        src="/public/contact_image-IJu_19v_.png"
+      />
+      <div className=" flex flex-col justify-center items-start gap-6">
+        <p className=" font-semibold text-lg text-gray-600"> OUR OFFICE</p>
+        <p className="text-gray-500">
+          00000 Willms Station <br />
+          Suite 000, Washington, USA
+        </p>
+        <p className="text-gray-500">
+          Tel: (000) 000-0000 <br />
+          Email: greatstackdev@gmail.com
+        </p>
+        <p className=" font-semibold text-lg text-gray-600">
+          CAREERS AT PRESCRIPTO
+        </p>
+        <p className="text-gray-500" >
+         
+          Learn more about our teams and job openings.
+        </p>
+        <button className=" border border-black px-8 py-4 text-sm  hover:bg-black hover:text-white transition-all duration-500"> Explore Jobs</button>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default Contact;
